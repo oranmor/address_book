f6872a9f2562f382f2432071a9270b249ec665a9
Denis Kurochkin <deniskuro@gmail.com>
2015-11-07 23:13:45 +0500

    Flash messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,51 +1,43 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :edit, :update, :destroy, :share]
 
-  # GET /contacts
   def index
     @contacts = Contact.all
   end
 
-  # GET /contacts/1
   def show
   end
 
-  # GET /contacts/new
   def new
     @contact = Contact.new
   end
 
-  # GET /contacts/1/edit
   def edit
   end
 
-  # POST /contacts
   def create
     @contact = Contact.new(contact_params)
 
     if @contact.save
-      redirect_to @contact, notice: 'Contact was successfully created.'
+      redirect_to @contact, notice: t('.notice', resource: Contact.model_name.human)
     else
       render :new
     end
   end
 
-  # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
-      redirect_to @contact, notice: 'Contact was successfully updated.'
+      redirect_to @contact, notice: t('.notice', resource: Contact.model_name.human)
     else
       render :edit
     end
   end
 
-  # DELETE /contacts/1
   def destroy
     @contact.destroy
-    redirect_to contacts_url, notice: 'Contact was successfully destroyed.'
+    redirect_to contacts_url, notice: t('.notice', resource: Contact.model_name.human)
   end
 
-  # PATCH /contacts/1/share
   def share
     ContactSharingMailer.share_contact(@contact.id, params.require(:email)).deliver_later
     redirect_to :back
@@ -53,12 +45,10 @@ class ContactsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_contact
     @contact = Contact.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def contact_params
     params.require(:contact).permit(:first_name, :last_name, emails: [], phones: [])
   end
