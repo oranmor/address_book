167242bbc0a7cefb97d4407eadffd0672be8edaa
Denis Kurochkin <deniskuro@gmail.com>
2015-11-07 20:20:28 +0500

    Contact model validates

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :emails, presence: true, if: ->(contact) { contact.phones.blank? }
+  validates :phones, presence: true, if: ->(contact) { contact.emails.blank? }
+  validates :first_name, uniqueness: { scope: :last_name }
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,3 +1,26 @@
-RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Contact, type: :model do
+  subject { create(:contact) }
+
+  it { is_expected.to be_valid }
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
+  it { is_expected.to validate_presence_of :emails }
+  it { is_expected.to validate_presence_of :phones }
+  it { is_expected.to validate_uniqueness_of(:first_name).scoped_to(:last_name) }
+
+  describe 'validations' do
+    context 'without phones, with emails' do
+      subject { build(:contact, phones: nil) }
+      it { is_expected.to be_valid }
+    end
+    context 'with phones, without emails' do
+      subject { build(:contact, emails: nil) }
+      it { is_expected.to be_valid }
+    end
+    context 'without phones, without emaisl' do
+      subject { build(:contact, emails: nil, phones: nil) }
+      it { is_expected.to_not be_valid }
+    end
+  end
+
 end
